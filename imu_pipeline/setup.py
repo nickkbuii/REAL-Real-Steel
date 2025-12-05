8f561a815e51546103a978437ae181a4f7268aa9
nickkbuii <nicholasqb01@gmail.com>
2025-12-04 20:40:06 -0800

    Setting up ROS side

---
+++ imu_pipeline/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = "imu_pipeline"
+
+setup(
+    name=package_name,
+    version="0.0.1",
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/dual_imu_filter.launch.py']),
+        ('share/' + package_name + '/config', [
+            'config/upper_filter.yaml',
+            'config/forearm_filter.yaml'
+        ]),
+    ],
+    install_requires=['setuptools'],
+    entry_points={
+        'console_scripts': [
+            'ble_imu_bridge = imu_pipeline.ble_imu_bridge:main'
+        ],
+    },
+)
